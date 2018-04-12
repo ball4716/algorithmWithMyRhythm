4fa6f309be5a42d47fe12a685e95913767f4eff1
HyunyoungKim <HyunyoungKim@DESKTOP-G5G3QI8>
2018-04-12 21:53:52 +0900

    2749 피보나치 수열에서 0과 1의 출력횟수 각각 구하기
    문제점: 메모이제이션을 사용했는데, 숫자가 클때 재귀에서 StackOverflow가 발생한다.
    해결해야함.

--- src/Solution_2749.java
+++ src/Solution_2749.java
@@ -1,17 +1,18 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Solution_2749 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num = 1000000;
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int fi[] = new int[n+1];
+		BigInteger n = new BigInteger(sc.next());
+		BigInteger fi[] = new BigInteger[n.add(BigInteger.ONE)]();
 		fi[0] = 0;
 		fi[1] = 1;
-		for(int i = 2; i <= n; i++) {
+		for(long i = 2; i <= n; i++) {
 			fi[i] = (fi[i-1]%num + fi[i-2]%num)%num;
 		}
-		System.out.println(fi[n]);
+		System.out.println(fi[n]%num);
 		sc.close();
 	}
 
